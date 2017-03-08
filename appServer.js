416999b50a8175058f050e0c66e201b51b57a258
mouryaamit <0821it071006>
2017-03-08 23:55:00 +0530

    initial

--- appServer.js
+++ appServer.js
@@ -6,7 +6,7 @@
 
     var loadMultipleDatabases = require('./nodedb/databases/databaseManager');
 
-    var port = 3814;
+    var port = process.env.PORT || 3814;
 	
 	if(!port){
 		console.log("Please specify the Port for application")
